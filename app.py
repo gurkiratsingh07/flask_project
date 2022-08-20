24e0310f1569bbfd00b136390bc440b33f14fde1
Gurkirat singh <mr.gurkiratsing@mail.com>
2022-08-20 10:04:02 +0530

    made some variable changes.

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ from db import db
 
 
 app=Flask(__name__)
-uri=os.getenv("DATABASE_URL")
-if uri.startswith("postgres://"):
-        uri=uri.replace("postgres://","postgresql://",1)
+uril=os.getenv("DATABASE_URL")
+if uril.startswith("postgres://"):
+        uril=uril.replace("postgres://","postgresql://",1)
 app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('uri','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.config['PROPAGATE_EXCEPTIONS']=True
